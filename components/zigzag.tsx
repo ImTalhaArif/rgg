cddb789d22c9d4d86ddf4eb33ea92eecbc73ce09
ImTalhaArif <62965957+ImTalhaArif@users.noreply.github.com>
2025-12-07 22:17:12 +0500

    Update zigzag.tsx

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -9,7 +9,7 @@ export default function Zigzag() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
 
         {/* Section header */}
-        <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+        <div id="services" className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
           <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">
             Reliable services under one roof
           </div>
